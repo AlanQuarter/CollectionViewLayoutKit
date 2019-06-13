0228c1fa5aa9aa778e727a661645f46110f0e8d4
AlanQuarter <the9thstar@naver.com>
2019-06-13 16:37:17 +0900

    Add a Cocoa Touch Framework target.

---
+++ CollectionViewLayoutKit/Sources/Sources.h
@@ -0,0 +1,19 @@
+//
+//  Sources.h
+//  Sources
+//
+//  Created by Alan on 13/06/2019.
+//  Copyright © 2019 Frog Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sources.
+FOUNDATION_EXPORT double SourcesVersionNumber;
+
+//! Project version string for Sources.
+FOUNDATION_EXPORT const unsigned char SourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sources/PublicHeader.h>
+
+
