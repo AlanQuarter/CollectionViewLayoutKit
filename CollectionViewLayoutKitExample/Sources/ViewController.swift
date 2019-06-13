2a3589ecd0d0f8d61f02ab0dec5842f19d14a851
AlanQuarter <the9thstar@naver.com>
2019-06-13 15:34:55 +0900

    Setup project.

---
+++ CollectionViewLayoutKitExample/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Alan on 13/06/2019.
+//  Copyright © 2019 Frog Lab. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
