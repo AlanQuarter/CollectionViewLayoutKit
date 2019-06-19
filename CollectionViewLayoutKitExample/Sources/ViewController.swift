bba6091b19abd8e2e25842f67c1e8cb713f40bad
AlanQuarter <the9thstar@naver.com>
2019-06-19 21:30:36 +0900

    Implements an example for the Waterfall Layout.

--- CollectionViewLayoutKitExample/Sources/ViewController.swift
+++ CollectionViewLayoutKitExample/Sources/ViewController.swift
@@ -1,49 +1,41 @@
 //
-//  Created by Alan on 13/06/2019.
-//  Copyright © 2019 Frog Lab. All rights reserved.
+// Created by Alan on 2019-06-19.
+// Copyright (c) 2019 Frog Lab. All rights reserved.
 //
 
 
+import Foundation
 import UIKit
-import CollectionViewLayoutKit
 
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    private weak var collectionView: UICollectionView!
+    private let menuCellIdentifier = "menuCell"
+
+
+    private weak var tableView: UITableView!
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = .white
+        self.navigationItem.title = "CollectionViewLayoutKit"
 
-        self.collectionView = {
-            var frame = UIScreen.main.bounds
-            frame.origin.y = 64
-            frame.size.height -= frame.origin.y
-
-            let layout = WaterfallLayout(
-                    itemWidth: { _ in .numberOfColumns(Int.random(in: 2 ... 3)) },
-                    itemHeight: { _ in 100 + CGFloat(Int.random(in: 0 ... 100)) })
-                    .setItemSpacing(width: 10, height: 10).setSectionInsets { _, _, section in section == 0 ? .zero : UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0) }
-                    .setNumberOfSectionHeader(Int.random(in: 1 ... 3))
-                    .setSectionHeaderHeight { _ in CGFloat(Int.random(in: 1 ... 2)) * 40 }
-                    .setNumberOfSectionFooter(Int.random(in: 1 ... 3))
-                    .setSectionHeaderInsets(top: 0, left: 0, bottom: 10, right: 0)
-                    .setSectionFooterHeight { _ in CGFloat(Int.random(in: 1 ... 2)) * 40 }
-                    .setSectionFooterInsets(top: 10, left: 0, bottom: 0, right: 0)
-
-            let view = UICollectionView(frame: frame, collectionViewLayout: layout)
-            view.backgroundColor = .yellow
-            view.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-            view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-            view.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
-            view.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "footer")
+        self.tableView = {
+            let view = UITableView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.register(UITableViewCell.self, forCellReuseIdentifier: self.menuCellIdentifier)
             view.dataSource = self
             view.delegate = self
 
             self.view.addSubview(view)
+
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+            ])
 
             return view
         }()
@@ -52,86 +44,59 @@ class ViewController: UIViewController {
 }
 
 
-extension ViewController: UICollectionViewDataSource {
+extension ViewController: UITableViewDataSource {
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Menu.allCases.count
     }
 
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let red = Int.random(in: 0 ... 200)
-        let green = Int.random(in: 0 ... 200)
-        let blue = Int.random(in: 0 ... 200)
-
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.contentView.backgroundColor = UIColor(
-                red: CGFloat(red) / 255,
-                green: CGFloat(green) / 255,
-                blue: CGFloat(blue) / 255,
-                alpha: 1
-        )
-
-        if let label = cell.contentView.viewWithTag(99) as? UILabel {
-            label.frame = cell.bounds
-            label.textColor = UIColor(
-                    red: CGFloat(255 - red) / 255,
-                    green: CGFloat(255 - green) / 255,
-                    blue: CGFloat(255 - blue) / 255,
-                    alpha: 1
-            )
-            label.text = "\(indexPath.row)"
-        } else {
-            let label = UILabel(frame: cell.bounds)
-            label.tag = 99
-            label.textColor = UIColor(
-                    red: CGFloat(255 - red) / 255,
-                    green: CGFloat(255 - green) / 255,
-                    blue: CGFloat(255 - blue) / 255,
-                    alpha: 1
-            )
-            label.text = "\(indexPath.row)"
-            label.textAlignment = .center
-            cell.contentView.addSubview(label)
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.menuCellIdentifier, for: indexPath)
+        cell.textLabel?.text = Menu.allCases[indexPath.row].description
 
         return cell
-    }
-
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-
-
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let supplementaryView: UICollectionReusableView
-
-        switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
-            sectionHeader.backgroundColor = .red
-            supplementaryView = sectionHeader
-
-        case UICollectionView.elementKindSectionFooter:
-            let sectionFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)
-            sectionFooter.backgroundColor = .blue
-            supplementaryView = sectionFooter
-
-        default:
-            supplementaryView = UICollectionReusableView()
-        }
-
-        return supplementaryView
     }
 
 }
 
 
-extension ViewController: UICollectionViewDelegate {
+extension ViewController: UITableViewDelegate {
 
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.collectionViewLayout.invalidateLayout()
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.navigationController?.pushViewController(Menu.allCases[indexPath.row].viewController, animated: true)
+    }
+
+}
+
+
+extension ViewController {
+
+    enum Menu: String, CaseIterable, CustomStringConvertible {
+
+        case waterfall = "Waterfall Layout"
+
+
+        static let allCases: [Menu] = [.waterfall]
+
+
+        var description: String {
+            return self.rawValue
+        }
+
+        var viewController: UIViewController {
+            let viewController: UIViewController
+
+            switch self {
+            case .waterfall:
+                viewController = WaterfallViewController()
+            }
+
+            viewController.navigationItem.title = self.description
+            return viewController
+        }
+
     }
 
 }
---
+++ CollectionViewLayoutKitExample/Sources/WaterfallViewController.swift
@@ -0,0 +1,145 @@
+//
+//  Created by Alan on 13/06/2019.
+//  Copyright © 2019 Frog Lab. All rights reserved.
+//
+
+
+import UIKit
+import CollectionViewLayoutKit
+
+
+class WaterfallViewController: UIViewController {
+
+    private let cellIdentifier = "cell"
+    private let sectionHeaderIdentifier = "header"
+    private let sectionFooterIdentifier = "footer"
+
+    private let cellLabelTag = 17
+    private let supplementaryViewLabelTag = 8376
+
+
+    private weak var collectionView: UICollectionView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView = {
+            let layout = WaterfallLayout(itemWidth: .numberOfColumns(2), itemHeight: { _ in CGFloat(Int.random(in: 100 ... 200)) })
+                    .setItemSpacing(width: 10, height: 10)
+                    .setSectionInsets { _, _, section in section == 0 ? .zero : UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0) }
+                    .setNumberOfSectionHeader { _, _, section in section + 1 }
+                    .setSectionHeaderHeight { _ in CGFloat(Int.random(in: 40 ... 100)) }
+                    .setSectionHeaderInsets(top: 0, left: 0, bottom: 10, right: 0)
+                    .setNumberOfSectionFooter { _, _, section in 2 - section }
+                    .setSectionFooterHeight { _ in CGFloat(Int.random(in: 40 ... 100)) }
+                    .setSectionFooterInsets(top: 10, left: 0, bottom: 0, right: 0)
+
+            let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.backgroundColor = .white
+            view.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: self.cellIdentifier)
+            view.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: self.sectionHeaderIdentifier)
+            view.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: self.sectionFooterIdentifier)
+            view.dataSource = self
+
+            self.view.addSubview(view)
+
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+            ])
+
+            return view
+        }()
+    }
+
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        self.collectionView.reloadData()
+    }
+
+}
+
+
+extension WaterfallViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor(
+                red: CGFloat(Int.random(in: 0 ... 255)) / 255,
+                green: CGFloat(Int.random(in: 0 ... 255)) / 255,
+                blue: CGFloat(Int.random(in: 0 ... 255)) / 255,
+                alpha: 0.6
+        )
+
+        if let label = cell.contentView.viewWithTag(self.cellLabelTag) as? UILabel {
+            label.frame = cell.bounds
+            label.text = "\(indexPath.row)"
+        } else {
+            let label = UILabel(frame: cell.bounds)
+            label.tag = self.cellLabelTag
+            label.textAlignment = .center
+            label.text = "\(indexPath.row)"
+
+            cell.contentView.addSubview(label)
+        }
+
+        return cell
+    }
+
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let supplementaryView: UICollectionReusableView
+        let identifier: String
+
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            identifier = self.sectionHeaderIdentifier
+
+            let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: identifier, for: indexPath)
+            sectionHeader.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+            supplementaryView = sectionHeader
+
+        case UICollectionView.elementKindSectionFooter:
+            identifier = self.sectionFooterIdentifier
+
+            let sectionFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: identifier, for: indexPath)
+            sectionFooter.backgroundColor = UIColor.blue.withAlphaComponent(0.5)
+            supplementaryView = sectionFooter
+
+        default:
+            identifier = ""
+            supplementaryView = UICollectionReusableView()
+        }
+
+        if let label = supplementaryView.viewWithTag(self.supplementaryViewLabelTag) as? UILabel {
+            label.frame = supplementaryView.bounds
+            label.text = identifier
+        } else {
+            let label = UILabel(frame: supplementaryView.bounds)
+            label.tag = self.supplementaryViewLabelTag
+            label.textAlignment = .center
+            label.text = identifier
+
+            supplementaryView.addSubview(label)
+        }
+
+        return supplementaryView
+    }
+
+}
